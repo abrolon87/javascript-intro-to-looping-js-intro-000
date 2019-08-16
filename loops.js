92d72d9f9727a06e6c5f78ddaf966d44a8eb5dc0
Amanda R <abrolon87@gmail.com>
2019-08-16 18:19:19 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,3 +17,13 @@ function whileLoop(n) {
    return "done";
 }
 //passed
+function doWhileLoop(num) {
+  var i = 0;
+  function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+  do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < 1);
+}
